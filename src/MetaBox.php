4f37ed5a42aed57f5d17aa3e871d51b3e9941436
setcookie <set@cooki.me>
2021-01-21 10:31:06 +0100

    introduce deselect function

--- src/MetaBox.php
+++ src/MetaBox.php
@@ -67,12 +67,25 @@ class MetaBox
         }
 
         ob_start(); ?>
-        <p style="display: none"><label for="role_visibility_select"><?php _e('Show only for roles', ROLE_VISIBILITY_DOMAIN); ?>:</label></p>
-        <select name="role_visibility_select[]" id="role_visibility_select" class="postbox" size="5" multiple>
-            <?php foreach(get_editable_roles() as $key => $role) { ?>
-                <option value="<?php echo $key; ?>" <?php echo ((in_array($key, $roles)) ? 'selected="selected"' : ''); ?>><?php echo $role['name']; ?></option>
-            <?php } ?>
-        </select>
+        <script type="text/javascript">
+            ((function($){
+                $(document).ready(function(){
+                    $('#role_visibility').find('a[data-action="deselect"]').on('click', function(e){
+                        e.preventDefault();
+                        $('#role_visibility_select').find('option:selected').prop('selected', false);
+                    });
+                });
+            }))(jQuery.noConflict());
+        </script>
+        <div id="role_visibility">
+            <p style="display: none"><label for="role_visibility_select"><?php _e('Show only for roles', ROLE_VISIBILITY_DOMAIN); ?>:</label></p>
+            <select name="role_visibility_select[]" id="role_visibility_select" class="postbox" size="5" multiple>
+                <?php foreach(get_editable_roles() as $key => $role) { ?>
+                    <option value="<?php echo $key; ?>" <?php echo ((in_array($key, $roles)) ? 'selected="selected"' : ''); ?>><?php echo $role['name']; ?></option>
+                <?php } ?>
+            </select>
+            <a href="javascript:void(0);" data-action="deselect"><?php _e('Deselect', ROLE_VISIBILITY_DOMAIN); ?></a>
+        </div>
         <?php echo ob_get_clean();
     }
 }
